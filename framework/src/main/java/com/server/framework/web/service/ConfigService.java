df7b40181b1a9a8c0fb1a1024fd9d26e9cd6bb0e
fengqingguo <295201976@qq.com>
2019-02-25 19:36:40 +0800

    框架搭建

---
+++ framework/src/main/java/com/server/framework/web/service/ConfigService.java
@@ -0,0 +1,28 @@
+package com.server.framework.web.service;
+
+import com.server.system.service.ISysConfigService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * server首创 html调用 thymeleaf 实现参数管理
+ * 
+ * @author server
+ */
+@Service("config")
+public class ConfigService
+{
+    @Autowired
+    private ISysConfigService configService;
+
+    /**
+     * 根据键名查询参数配置信息
+     * 
+     * @param configName 参数名称
+     * @return 参数键值
+     */
+    public String getKey(String configKey)
+    {
+        return configService.selectConfigByKey(configKey);
+    }
+}
---
+++ views/src/main/java/com/server/web/controller/monitor/SysUserOnlineController.java
@@ -0,0 +1,117 @@
+package com.server.web.controller.monitor;
+
+import java.util.List;
+
+import com.server.shiro.session.OnlineSession;
+import com.server.shiro.session.OnlineSessionDAO;
+import com.server.shiro.util.ShiroUtils;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.server.common.annotation.Log;
+import com.server.common.base.AjaxResult;
+import com.server.common.enums.BusinessType;
+import com.server.common.enums.OnlineStatus;
+import com.server.common.page.TableDataInfo;
+import com.server.shiro.session.OnlineSession;
+import com.server.shiro.session.OnlineSessionDAO;
+import com.server.system.domain.SysUserOnline;
+import com.server.system.service.ISysUserOnlineService;
+import com.server.framework.web.base.BaseController;
+
+/**
+ * 在线用户监控
+ * 
+ * @author server
+ */
+@Controller
+@RequestMapping("/monitor/online")
+public class SysUserOnlineController extends BaseController
+{
+    private String prefix = "monitor/online";
+
+    @Autowired
+    private ISysUserOnlineService userOnlineService;
+
+    @Autowired
+    private OnlineSessionDAO onlineSessionDAO;
+
+    @RequiresPermissions("monitor:online:view")
+    @GetMapping()
+    public String online()
+    {
+        return prefix + "/online";
+    }
+
+    @RequiresPermissions("monitor:online:list")
+    @PostMapping("/list")
+    @ResponseBody
+    public TableDataInfo list(SysUserOnline userOnline)
+    {
+        startPage();
+        List<SysUserOnline> list = userOnlineService.selectUserOnlineList(userOnline);
+        return getDataTable(list);
+    }
+
+    @RequiresPermissions("monitor:online:batchForceLogout")
+    @Log(title = "在线用户", businessType = BusinessType.FORCE)
+    @PostMapping("/batchForceLogout")
+    @ResponseBody
+    public AjaxResult batchForceLogout(@RequestParam("ids[]") String[] ids)
+    {
+        for (String sessionId : ids)
+        {
+            SysUserOnline online = userOnlineService.selectOnlineById(sessionId);
+            if (online == null)
+            {
+                return error("用户已下线");
+            }
+            OnlineSession onlineSession = (OnlineSession) onlineSessionDAO.readSession(online.getSessionId());
+            if (onlineSession == null)
+            {
+                return error("用户已下线");
+            }
+            if (sessionId.equals(ShiroUtils.getSessionId()))
+            {
+                return error("当前登陆用户无法强退");
+            }
+            onlineSession.setStatus(OnlineStatus.off_line);
+            onlineSessionDAO.update(onlineSession);
+            online.setStatus(OnlineStatus.off_line);
+            userOnlineService.saveOnline(online);
+        }
+        return success();
+    }
+
+    @RequiresPermissions("monitor:online:forceLogout")
+    @Log(title = "在线用户", businessType = BusinessType.FORCE)
+    @PostMapping("/forceLogout")
+    @ResponseBody
+    public AjaxResult forceLogout(String sessionId)
+    {
+        SysUserOnline online = userOnlineService.selectOnlineById(sessionId);
+        if (sessionId.equals(ShiroUtils.getSessionId()))
+        {
+            return error("当前登陆用户无法强退");
+        }
+        if (online == null)
+        {
+            return error("用户已下线");
+        }
+        OnlineSession onlineSession = (OnlineSession) onlineSessionDAO.readSession(online.getSessionId());
+        if (onlineSession == null)
+        {
+            return error("用户已下线");
+        }
+        onlineSession.setStatus(OnlineStatus.off_line);
+        onlineSessionDAO.update(onlineSession);
+        online.setStatus(OnlineStatus.off_line);
+        userOnlineService.saveOnline(online);
+        return success();
+    }
+}
